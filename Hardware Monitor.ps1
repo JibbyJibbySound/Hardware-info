43eec41dd113fd4bcc346ed2aca0f0bcd30d1600
JibbyJibbySound <105805500+JibbyJibbySound@users.noreply.github.com>
2022-06-01 13:32:48 +0100

    Update Hardware Monitor.ps1
    Added functionality for Driver checking
    Added functionality for Video Controller
    Made memory info programatic
    Fixed Memory headers

--- Hardware Monitor.ps1
+++ Hardware Monitor.ps1
@@ -1,12 +1,14 @@
 <# Hardware Info Script #>
 <#
     Author: Gabriel Plume
-    Version: 0.2
-    Date uploaded: 31/05/2022
+    Version: 0.3
+    Date uploaded: 01/06/2022
     Change note:
-        Added functionality to capacity button
+        Added functionality for Driver checking
+        Added functionality for Video Controller
+        Made memory info programatic
+        Fixed Memory headers
 #>
-$Cs=(Get-CimInstance -ClassName CIM_ComputerSystem | Select-Object Name).Name
 
 $Cs=(Get-CimInstance -ClassName CIM_ComputerSystem | Select-Object Name).Name
 
@@ -20,10 +22,11 @@ $Cs=(Get-CimInstance -ClassName CIM_ComputerSystem | Select-Object Name).Name
         <Label Name="ComputerConnected" Content="Connected to $Cs" Canvas.Left="5" Canvas.Top="5"/>
         <Label Name="Auth" Content="By Gabriel Plume" Canvas.Bottom="5" Canvas.Right="10"/>
         <Button Name="Reconnect" Canvas.Bottom="8" Canvas.Left="10" HorizontalAlignment="Center">Connect to other</Button>
-        <Grid Width="{Binding ElementName=Panel1, Path=ActualWidth}" Canvas.Top="85">
+        <Grid Width="{Binding ElementName=Panel1, Path=ActualWidth}" Canvas.Top="35">
             <Grid.RowDefinitions>
                 <RowDefinition/>
-                <RowDefinition Height="35"/>
+                <RowDefinition Height="18"/>
+                <RowDefinition Height="60"/>
                 <RowDefinition Height="60"/>
                 <RowDefinition Height="60"/>
                 <RowDefinition Height="60"/>
@@ -33,11 +36,13 @@ $Cs=(Get-CimInstance -ClassName CIM_ComputerSystem | Select-Object Name).Name
             <Button Name="CPU" Grid.Row="2" Height="50" Width="150" HorizontalAlignment="Center">CPU Info</Button>
             <Button Name="Memory" Grid.Row="3" Height="50" Width="150" HorizontalAlignment="Center">Memory Info</Button>
             <Button Name="Storage" Grid.Row="4" Height="50" Width="150" HorizontalAlignment="Center">Storage Info</Button>
-            <Button Grid.Row="5" Height="50" Width="150" HorizontalAlignment="Center">Test2</Button>
+            <Button Name="Video" Grid.Row="5" Height="50" Width="150" HorizontalAlignment="Center">Video Controller Info</Button>
+            <Button Name="Drivers" Grid.Row="6" Height="50" Width="150" HorizontalAlignment="Center">Driver Info</Button>
         </Grid>
     </Canvas>
 </Window>
 "@
+
 $MenuNR=(New-Object System.Xml.XmlNodeReader $MenuForm)
 $MenuWin=[Windows.Markup.XamlReader]::Load( $MenuNR )
 
@@ -45,6 +50,8 @@ $Device = $MenuWin.FindName("ComputerConnected")
 $CPU = $MenuWin.FindName("CPU")
 $Memory = $MenuWin.FindName("Memory")
 $Storage = $MenuWin.FindName("Storage")
+$Video = $MenuWin.FindName("Video")
+$Drivers = $MenuWin.FindName("Drivers")
 $Reconnect = $MenuWin.FindName("Reconnect")
 
 <# CPU information window #>
@@ -99,10 +106,9 @@ $Man = $CPUWin.FindName("Man")
 [xml]$MMForm = @"
 <Window
     xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
-    Title="Main Memory Info" Height="100" Width="600" ResizeMode="NoResize">
-    <Grid Width="{Binding ElementName=Panel1, Path=ActualWidth}">
+    Title="Main Memory Info" Height="100" Width="800" ResizeMode="NoResize">
+    <Grid Name="MMGrid" Width="{Binding ElementName=Panel1, Path=ActualWidth}">
         <Grid.RowDefinitions>
-            <RowDefinition/>
             <RowDefinition/>
         </Grid.RowDefinitions>
         <Grid.ColumnDefinitions>
@@ -121,13 +127,6 @@ $Man = $CPUWin.FindName("Man")
         <Label Grid.Column="4">Speed</Label>
         <Label Grid.Column="5">Data Width</Label>
 
-        <Label Name="PN" Grid.Row="1" Grid.Column="0"> </Label>
-        <Label Name="MMMan" Grid.Row="1" Grid.Column="1"> </Label>
-        <Label Name="Location" Grid.Row="1" Grid.Column="2"> </Label>
-        <Label Name="Cap" Grid.Row="1" Grid.Column="3"> </Label>
-        <Label Name="Speed" Grid.Row="1" Grid.Column="4"> </Label>
-        <Label Name="DW" Grid.Row="1" Grid.Column="5"> </Label>
-
     </Grid>
 </Window>
 "@
@@ -135,15 +134,10 @@ $Man = $CPUWin.FindName("Man")
 $MMNR=(New-Object System.Xml.XmlNodeReader $MMForm)
 $MMWin=[Windows.Markup.XamlReader]::Load( $MMNR )
 
+$MMGrid = $MMWin.FindName("MMGrid")
+
 $MMWin.Add_Closing({$_.Cancel = $true
                     $MMWin.Hide()})
-
-$PN = $MMWin.FindName("PN")
-$MMMan = $MMWin.FindName("MMMan")
-$Location = $MMWin.FindName("Location")
-$Cap = $MMWin.FindName("Cap")
-$Speed = $MMWin.FindName("Speed")
-$DW = $MMWin.FindName("DW")
 
 <# Capacity Info window #>
 
@@ -181,7 +175,83 @@ $CapWin.Add_Closing({$_.Cancel = $true
 
 $CapGrid = $CapWin.FindName("CapGrid")
 
-# $CapGrid.ColumnDefinitions.Add()
+<# Video Controller Window #>
+
+[xml]$VidForm = @"
+<Window
+    xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+    Title="Video Controller Info" Height="150" Width="Auto">
+    <ScrollViewer HorizontalScrollBarVisibility="Auto">  
+        <Grid Name="VidGrid" Width="{Binding ElementName=Panel1, Path=ActualWidth}">
+            <Grid.RowDefinitions>
+                <RowDefinition Height="Auto"/>
+            </Grid.RowDefinitions>
+            <Grid.ColumnDefinitions>
+                <ColumnDefinition/>
+                <ColumnDefinition/>
+                <ColumnDefinition/>
+                <ColumnDefinition/>
+                <ColumnDefinition/>
+            </Grid.ColumnDefinitions>
+        
+            <Label Grid.Column="0">Name</Label>
+            <Label Grid.Column="1">Adapter Compatibility</Label>
+            <Label Grid.Column="2">Video Mode</Label>
+            <Label Grid.Column="3">Max Refresh Rate</Label>
+            <Label Grid.Column="4">Video Processor</Label>
+
+        </Grid>
+    </ScrollViewer>
+</Window>
+"@
+
+$VidNR=(New-Object System.Xml.XmlNodeReader $VidForm)
+$VidWin=[Windows.Markup.XamlReader]::Load( $VidNR )
+
+$VidWin.Add_Closing({$_.Cancel = $true
+                    $VidWin.Hide()})
+
+$VidGrid = $VidWin.FindName("VidGrid")
+
+<# Driver Window #>
+
+[xml]$DivForm = @"
+<Window
+    xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+    Title="Driver Info" Height="Auto" Width="Auto">
+    <ScrollViewer HorizontalScrollBarVisibility="Auto">  
+        <Grid Name="DivGrid" Width="{Binding ElementName=Panel1, Path=ActualWidth}">
+            <Grid.RowDefinitions>
+                <RowDefinition Height="Auto"/>
+            </Grid.RowDefinitions>
+            <Grid.ColumnDefinitions>
+                <ColumnDefinition/>
+                <ColumnDefinition/>
+                <ColumnDefinition/>
+                <ColumnDefinition/>
+                <ColumnDefinition/>
+                <ColumnDefinition/>
+            </Grid.ColumnDefinitions>
+        
+            <Label Grid.Column="0">File Location</Label>
+            <Label Grid.Column="1">Class Name</Label>
+            <Label Grid.Column="2">Boot Critical</Label>
+            <Label Grid.Column="3">Provider</Label>
+            <Label Grid.Column="4">Date</Label>
+            <Label Grid.Column="5">Version</Label>
+
+        </Grid>
+    </ScrollViewer>
+</Window>
+"@
+
+$DivNR=(New-Object System.Xml.XmlNodeReader $DivForm)
+$DivWin=[Windows.Markup.XamlReader]::Load( $DivNR )
+
+$DivWin.Add_Closing({$_.Cancel = $true
+                    $DivWin.Hide()})
+
+$DivGrid = $DivWin.FindName("DivGrid")
 
 <# Connect window #>
 
@@ -233,12 +303,59 @@ $Memory.Add_Click({
     }else{
     $Data = Get-CimInstance -ClassName CIM_PhysicalMemory -ComputerName $Cs
     }
-    $PN.Content = $Data.PartNumber
-    $MMMan.Content = $Data.Manufacturer
-    $Location.Content = $Data.DataWidth
-    $Cap.Content = $Data.Capacity
-    $Speed.Content = $Data.Speed
-    $DW.Content = $Data.DataWidth
+    
+    $Count=1
+
+    foreach ($d in $Data) {
+        $MMRow = new-object system.windows.controls.rowdefinition
+        $MMRow.Height="Auto"
+        $MMGrid.RowDefinitions.Add($MMRow)
+        
+        $PN = New-Object System.Windows.Controls.Label
+        $PN.Name = "PartNumber"
+        $PN.Content = $d.PartNumber
+        [System.Windows.Controls.Grid]::SetRow($PN,$Count)
+        [System.Windows.Controls.Grid]::SetColumn($PN,0)
+        $MMGrid.AddChild($PN)
+
+        $Manu = New-Object System.Windows.Controls.Label
+        $Manu.Name = "Manufacturer"
+        $Manu.Content = $d.Manufacturer
+        [System.Windows.Controls.Grid]::SetRow($Manu,$Count)
+        [System.Windows.Controls.Grid]::SetColumn($Manu,1)
+        $MMGrid.AddChild($Manu)
+
+        $Location = New-Object System.Windows.Controls.Label
+        $Location.Name = "DeviceLocator"
+        $Location.Content = $d.DeviceLocator
+        [System.Windows.Controls.Grid]::SetRow($Location,$Count)
+        [System.Windows.Controls.Grid]::SetColumn($Location,2)
+        $MMGrid.AddChild($Location)
+
+        $Cap = New-Object System.Windows.Controls.Label
+        $Cap.Name = "Capacity"
+        $Cap.Content = $d.Capacity
+        [System.Windows.Controls.Grid]::SetRow($Cap,$Count)
+        [System.Windows.Controls.Grid]::SetColumn($Cap,3)
+        $MMGrid.AddChild($Cap)
+
+        $Speed = New-Object System.Windows.Controls.Label
+        $Speed.Name = "Speed"
+        $Speed.Content = $d.Speed
+        [System.Windows.Controls.Grid]::SetRow($Speed,$Count)
+        [System.Windows.Controls.Grid]::SetColumn($Speed,4)
+        $MMGrid.AddChild($Speed)
+
+        $DW = New-Object System.Windows.Controls.Label
+        $DW.Name = "DataWidth"
+        $DW.Content = $d.DataWidth
+        [System.Windows.Controls.Grid]::SetRow($DW,$Count)
+        [System.Windows.Controls.Grid]::SetColumn($DW,5)
+        $MMGrid.AddChild($DW)
+
+        $Count++
+    }
+
     $MMWin.ShowDialog()
 })
 
@@ -366,6 +483,123 @@ $Storage.Add_Click({
     }
 
     $CapWin.ShowDialog()
+})
+
+$Video.Add_Click({
+    if($Cs -EQ (Get-CimInstance -ClassName CIM_ComputerSystem | Select-Object Name).Name){
+    $Data = Get-CimInstance -ClassName CIM_VideoController | Select-Object -Property Caption,AdapterCompatibility,VideoModeDescription,MaxRefreshRate,VideoProcessor
+    }else{
+    $Data = Get-CimInstance -ClassName CIM_VideoController | Select-Object -Property Caption,AdapterCompatibility,VideoModeDescription,MaxRefreshRate,VideoProcessor -ComputerName $Cs
+    }
+
+    $Count=1
+
+    foreach ($d in $Data) {
+        $CapRow = new-object system.windows.controls.rowdefinition
+        $CapRow.Height="Auto"
+        $VidGrid.RowDefinitions.Add($CapRow)
+        
+        $NameLabel = New-Object System.Windows.Controls.Label
+        $NameLabel.Name = "Name"
+        $NameLabel.Content = $d.Caption
+        [System.Windows.Controls.Grid]::SetRow($NameLabel,$Count)
+        [System.Windows.Controls.Grid]::SetColumn($NameLabel,0)
+        $VidGrid.AddChild($NameLabel)
+
+        $AC = New-Object System.Windows.Controls.Label
+        $AC.Name = "AdapterComp"
+        $AC.Content = $d.AdapterCompatibility
+        [System.Windows.Controls.Grid]::SetRow($AC,$Count)
+        [System.Windows.Controls.Grid]::SetColumn($AC,1)
+        $VidGrid.AddChild($AC)
+
+        $VMD = New-Object System.Windows.Controls.Label
+        $VMD.Name = "VideoModeDescription"
+        $VMD.Content = $d.VideoModeDescription
+        [System.Windows.Controls.Grid]::SetRow($VMD,$Count)
+        [System.Windows.Controls.Grid]::SetColumn($VMD,2)
+        $VidGrid.AddChild($VMD)
+
+        $MaxRR = New-Object System.Windows.Controls.Label
+        $MaxRR.Name = "MaxRefresh"
+        $MaxRR.Content = $d.MaxRefreshRate
+        [System.Windows.Controls.Grid]::SetRow($MaxRR,$Count)
+        [System.Windows.Controls.Grid]::SetColumn($MaxRR,3)
+        $VidGrid.AddChild($MaxRR)
+
+        $VP = New-Object System.Windows.Controls.Label
+        $VP.Name = "VidPro"
+        $VP.Content = $d.VideoProcessor
+        [System.Windows.Controls.Grid]::SetRow($VP,$Count)
+        [System.Windows.Controls.Grid]::SetColumn($VP,4)
+        $VidGrid.AddChild($VP)
+
+        $Count++
+    }
+
+    $VidWin.ShowDialog()
+})
+
+$Drivers.Add_Click({
+    if($Cs -EQ (Get-CimInstance -ClassName CIM_ComputerSystem | Select-Object Name).Name){
+    $Data = Get-WindowsDriver -Online |Sort-Object -Property ClassName |Select-Object -Property OriginalFileName,ClassName,BootCritical,ProviderName,Date,Version
+    }else{
+    $Data = Get-WindowsDriver -Online |Sort-Object -Property ClassName |Select-Object -Property OriginalFileName,ClassName,BootCritical,ProviderName,Date,Version -ComputerName $Cs
+    }
+
+    $Count=1
+
+    foreach ($d in $Data) {
+        $CapRow = new-object system.windows.controls.rowdefinition
+        $CapRow.Height="Auto"
+        $DivGrid.RowDefinitions.Add($CapRow)
+        
+        $FileLabel = New-Object System.Windows.Controls.Label
+        $FileLabel.Name = "FileName"
+        $FileLabel.Content = $d.OriginalFileName
+        [System.Windows.Controls.Grid]::SetRow($FileLabel,$Count)
+        [System.Windows.Controls.Grid]::SetColumn($FileLabel,0)
+        $DivGrid.AddChild($FileLabel)
+
+        $CN = New-Object System.Windows.Controls.Label
+        $CN.Name = "ClassName"
+        $CN.Content = $d.ClassName
+        [System.Windows.Controls.Grid]::SetRow($CN,$Count)
+        [System.Windows.Controls.Grid]::SetColumn($CN,1)
+        $DivGrid.AddChild($CN)
+
+        $BC = New-Object System.Windows.Controls.Label
+        $BC.Name = "BootCritical"
+        $BC.Content = $d.BootCritical
+        [System.Windows.Controls.Grid]::SetRow($BC,$Count)
+        [System.Windows.Controls.Grid]::SetColumn($BC,2)
+        $DivGrid.AddChild($BC)
+
+        $PN = New-Object System.Windows.Controls.Label
+        $PN.Name = "ProviderName"
+        $PN.Content = $d.ProviderName
+        [System.Windows.Controls.Grid]::SetRow($PN,$Count)
+        [System.Windows.Controls.Grid]::SetColumn($PN,3)
+        $DivGrid.AddChild($PN)
+
+        $Date = New-Object System.Windows.Controls.Label
+        $Date.Name = "Date"
+        $Date.Content = $d.Date
+        [System.Windows.Controls.Grid]::SetRow($Date,$Count)
+        [System.Windows.Controls.Grid]::SetColumn($Date,4)
+        $DivGrid.AddChild($Date)
+
+        $Vers = New-Object System.Windows.Controls.Label
+        $Vers.Name = "Version"
+        $Vers.Content = $d.Version
+        [System.Windows.Controls.Grid]::SetRow($Vers,$Count)
+        [System.Windows.Controls.Grid]::SetColumn($Vers,5)
+        $DivGrid.AddChild($Vers)
+
+        $Count++
+    }
+
+    $DivWin.ShowDialog()
 })
 
 $Reconnect.Add_Click({
